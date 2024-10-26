ddd280976b513427247d1f83222ac66225f82bfa
909 <turnerfelino@gmail.com>
2024-10-26 13:10:57 +1300

    more small format fixes

--- lua/colors/909.lua
+++ lua/colors/909.lua
@@ -1,24 +1,24 @@
 require('mini.base16').setup {
-    palette = {
-        base00 = '#101010',  -- Background
-        base01 = '#161616',  -- Lighter background
-        base02 = '#202020',  -- Selection background
-        base03 = '#635945',  -- Comments
-        base04 = '#80775d',  -- Darker foreground
-        base05 = '#aa9e7a',  -- Default foreground
-        base06 = '#d4be98',  -- Lighter foreground
-        base07 = '#e5d4a8',  -- Lightest foreground
-        base08 = '#fb4934',  -- Red (errors, highlights)
-        base09 = '#fe8019',  -- Orange (warnings)
-        base0A = '#fabd2f',  -- Yellow
-        base0B = '#b8bb26',  -- Green (success)
-        base0C = '#8ec07c',  -- Cyan
-        base0D = '#83a598',  -- Blue (info)
-        base0E = '#d3869b',  -- Magenta
-        base0F = '#d65d0e',  -- Brown (special warnings)
-    },
-    use_cterm = true,  -- This is optional; set it if you're using terminal Neovim
-    plugins = { default = true, },
+  palette = {
+    base00 = '#101010',  -- Background
+    base01 = '#161616',  -- Lighter background
+    base02 = '#202020',  -- Selection background
+    base03 = '#635945',  -- Comments
+    base04 = '#80775d',  -- Darker foreground
+    base05 = '#aa9e7a',  -- Default foreground
+    base06 = '#d4be98',  -- Lighter foreground
+    base07 = '#e5d4a8',  -- Lightest foreground
+    base08 = '#fb4934',  -- Red (errors, highlights)
+    base09 = '#fe8019',  -- Orange (warnings)
+    base0A = '#fabd2f',  -- Yellow
+    base0B = '#b8bb26',  -- Green (success)
+    base0C = '#8ec07c',  -- Cyan
+    base0D = '#83a598',  -- Blue (info)
+    base0E = '#d3869b',  -- Magenta
+    base0F = '#d65d0e',  -- Brown (special warnings)
+  },
+  use_cterm = true,  -- This is optional; set it if you're using terminal Neovim
+  plugins = { default = true, },
 }
 
 local hl = vim.api.nvim_set_hl
--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -3,17 +3,17 @@ return {
     "kdheepak/lazygit.nvim",
     lazy = true,
     cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
     },
     dependencies = {
-        "nvim-lua/plenary.nvim",
+      "nvim-lua/plenary.nvim",
     },
     keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     },
     config = function()
       vim.g.lazygit_floating_window_border_chars = {'┌','─', '┐', '│', '┘','─', '└', '│'}
