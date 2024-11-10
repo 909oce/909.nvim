ed4528a249518879f0fe05c6824d1dbbad39079d
909 <turnerfelino@gmail.com>
2024-11-10 20:57:31 +1300

    small formatting fix

--- init.lua
+++ init.lua
@@ -9,15 +9,9 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 
 require("lazy").setup(
-  {
-    {
-      import = "plugins"
-    },
-  },
-lazy_config)
-
-require "options"
-
-vim.schedule(function()
-  require "mappings"
-end)
+  {{import = "plugins"}})
+  require "options"
+  vim.schedule(function()
+    require "mappings"
+  end
+)
