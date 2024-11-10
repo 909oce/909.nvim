8e0328faf158a599faa985466c6a3ba2525197f8
909 <turnerfelino@gmail.com>
2024-11-10 20:56:54 +1300

    initial lsp setup

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,76 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "j-hui/fidget.nvim",
+  },
+
+  config = function()
+    require("fidget").setup()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "hyprls",
+        "lua_ls",
+        "cssls",
+        "html",
+        "intelephense",
+      },
+      handlers = {
+        function (server_name)
+          require("lspconfig")[server_name].setup {}
+        end,
+        ["lua_ls"] = function()
+          require("lspconfig").lua_ls.setup {
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" }
+                }
+              }
+            }
+          }
+        end,
+      }
+    })
+    local cmp = require("cmp")
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<C-Enter>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+      sources = require("cmp").config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+      }, {
+        { name = "buffer" },
+      })
+    })
+    vim.diagnostic.config({
+      update_in_insert = true,
+      float = {
+        focusable = false,
+        style = "minimal",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    })
+  end,
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,12 +3,24 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter").setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "elixir", "heex", "javascript", "html", "css", "hyprlang" },
-      sync_install = false,
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "python",
+        "php",
+        "heex",
+        "javascript",
+        "html",
+        "css",
+        "hyprlang"
+      },
+      sync_install = true,
       auto_install = true,
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = true,
       },
       indent = {
         enable = true,
