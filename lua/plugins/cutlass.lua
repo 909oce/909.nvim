6c4e9a31e659e47d707bb7aa45120f233aae1e71
909 <turnerfelino@gmail.com>
2024-11-13 14:36:12 +1300

    override del

--- lua/plugins/cutlass.lua
+++ lua/plugins/cutlass.lua
@@ -3,7 +3,8 @@ return {
     "gbprod/cutlass.nvim",
     config = function()
       require("cutlass").setup({
-        cut_key = "x"
+        cut_key = "x",
+        override_del = true
       })
     end
   }
