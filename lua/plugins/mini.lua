c6b10c543066e1e200b24c5ff30740318fdc1914
909 <turnerfelino@gmail.com>
2024-11-14 15:34:16 +1300

    nothing changed

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,33 +3,33 @@ return {
     "echasnovski/mini.nvim",
     config = function()
       require "colors.909"
-      require ("mini.statusline").setup({
+      require("mini.statusline").setup({
         use_icons = true,
         set_vim_settings = true
       })
-      require ("mini.statusline").section_location = function()
+      require("mini.statusline").section_location = function()
         return '%2l:%-2v'
       end
-      require ("mini.git").setup()
-      require ("mini.diff").setup({
+      require("mini.git").setup()
+      require("mini.diff").setup({
         view = {
           style = 'number'
         }
       })
-      require ("mini.pairs").setup()
-      require ("mini.tabline").setup({
+      require("mini.pairs").setup()
+      require("mini.tabline").setup({
         format = function(buf_id, label)
           local suffix = vim.bo[buf_id].modified and '+ ' or ''
           return require('mini.tabline').default_format(buf_id, label) .. suffix
         end,
       })
-      require ("mini.icons").setup()
-      require ("mini.hipatterns").setup({
+      require("mini.icons").setup()
+      require("mini.hipatterns").setup({
         highlighters = {
           hex_color = require('mini.hipatterns').gen_highlighter.hex_color(),
         },
       })
-      require ("mini.cursorword").setup()
+      require("mini.cursorword").setup()
     end,
   }
 }
