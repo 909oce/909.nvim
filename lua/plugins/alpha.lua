0f5213bdeb91f506d8debcf2b088936688bf1ffe
909 <turnerfelino@gmail.com>
2024-11-17 07:01:37 +1300

    formatting

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,9 +1,9 @@
 return {
   {
     "goolord/alpha-nvim",
-    config = function ()
-      local alpha = require'alpha'
-      local dashboard = require'alpha.themes.dashboard'
+    config = function()
+      local alpha = require 'alpha'
+      local dashboard = require 'alpha.themes.dashboard'
       dashboard.section.header.val = {
         [[                               __                ]],
         [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
@@ -19,10 +19,10 @@ return {
         -- [[╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
       }
       dashboard.section.buttons.val = {
-        dashboard.button( "e", "  New file" , ":ene<CR>"),
-        dashboard.button( "f", "󰈞  Find file", ":Ranger<CR>"),
+        dashboard.button("e", "  New file", ":ene<CR>"),
+        dashboard.button("f", "󰈞  Find file", ":Ranger<CR>"),
         dashboard.button("r", "󰊄  Recently opened files", "<cmd>Telescope oldfiles<CR>"),
-        dashboard.button( "q", "󰅚  Quit NVIM" , ":qa<CR>"),
+        dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
       }
       alpha.setup(dashboard.config)
     end
