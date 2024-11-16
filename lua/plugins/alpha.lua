18da976f3b4a5ba3863b0387dfe74db7860499f3
909 <turnerfelino@gmail.com>
2024-11-17 08:12:53 +1300

    add some variety to neovim logo and change dashboard highlights

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,29 +1,24 @@
 return {
   {
     "goolord/alpha-nvim",
+    dependencies = {
+      "MaximilianLloyd/ascii.nvim"
+    },
     config = function()
       local alpha = require 'alpha'
       local dashboard = require 'alpha.themes.dashboard'
-      dashboard.section.header.val = {
-        [[                               __                ]],
-        [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-        [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-        [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-        [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-        [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
-        -- [[███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗]],
-        -- [[████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║]],
-        -- [[██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║]],
-        -- [[██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
-        -- [[██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║]],
-        -- [[╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
-      }
+      dashboard.section.header.val = require("ascii").get_random("text", "neovim")
       dashboard.section.buttons.val = {
         dashboard.button("e", "  New file", ":ene<CR>"),
         dashboard.button("f", "󰈞  Find file", ":Ranger<CR>"),
         dashboard.button("r", "󰊄  Recently opened files", "<cmd>Telescope oldfiles<CR>"),
         dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
       }
+      dashboard.section.header.opts.hl = "AlphaHeader"
+      for _, button in ipairs(dashboard.section.buttons.val) do
+        button.opts.hl = "AlphaButton"
+        button.opts.hl_shortcut = "AlphaButton"
+      end
       alpha.setup(dashboard.config)
     end
   }
