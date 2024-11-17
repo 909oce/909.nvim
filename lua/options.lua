818483d9970eb89b0abb56d5d0ca4a7d54adfe0b
909 <turnerfelino@gmail.com>
2024-11-18 06:53:00 +1300

    disbale whichrap for now

--- lua/options.lua
+++ lua/options.lua
@@ -42,4 +42,4 @@ o.updatetime = 250
 
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
-opt.whichwrap:append "<>[]hl"
+-- opt.whichwrap:append "<>[]hl"
