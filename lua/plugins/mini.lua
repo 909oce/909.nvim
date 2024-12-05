fe5e4e7c447cb53794bc7a939e9c31aebe5d5764
909 <turnerfelino@gmail.com>
2024-12-06 07:45:14 +1300

    some statusline fixes to show lsp in a minimal way

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -9,19 +9,35 @@ return {
         content = {
           active = function()
             local MiniStatusline = require "mini.statusline"
+
+            MiniStatusline.section_lsp = function(args)
+              if MiniStatusline.is_truncated(args.trunc_width) then
+                return ""
+              end
+
+              local buf_clients = vim.lsp.get_clients { bufnr = vim.api.nvim_get_current_buf() }
+              if #buf_clients == 0 then
+                return ""
+              end
+
+              local icon = args.icon or (MiniStatusline.config.use_icons and "󰰎" or "LSP")
+              return icon
+            end
+
+            MiniStatusline.section_location = function()
+              return "%2l:%-2v"
+            end
+
             local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 0 }
-            -- local git = MiniStatusline.section_git { trunc_width = 0 }
-            -- local diff = MiniStatusline.section_diff { trunc_width = 0 }
-            -- local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 0 }
-            -- local lsp = MiniStatusline.section_lsp { trunc_width = 0 }
+            local lsp = MiniStatusline.section_lsp { trunc_width = 0 }
             local filename = MiniStatusline.section_filename { trunc_width = 75 }
             local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 100 }
-            local location = MiniStatusline.section_location { trunc_width = 0 }
+            local location = MiniStatusline.section_location()
             local search = MiniStatusline.section_searchcount { trunc_width = 0 }
 
             return MiniStatusline.combine_groups {
               { hl = mode_hl, strings = { mode } },
-              -- { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+              { hl = "MiniStatuslineDevinfo", strings = { lsp } },
               "%<", -- Mark general truncate point
               { hl = "MiniStatuslineFilename", strings = { filename } },
               "%=", -- End left alignment
@@ -31,9 +47,6 @@ return {
           end,
         },
       }
-      require("mini.statusline").section_location = function()
-        return "%2l:%-2v"
-      end
       require("mini.git").setup()
       require("mini.diff").setup {
         view = {
