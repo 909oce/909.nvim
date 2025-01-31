cb1b6920b330e8e746bc5bee2c44779ad1e4953e
909 <turnerfelino@gmail.com>
2025-02-01 09:04:32 +1300

    update cord and fix config

--- lua/plugins/cord.lua
+++ lua/plugins/cord.lua
@@ -1,19 +1,17 @@
 return {
   {
     "vyfor/cord.nvim",
-    build = "./build || .\\build",
-    event = "VeryLazy",
+    build = ":Cord update",
     config = function()
       require("cord").setup {
-        usercmds = true,
         log_level = "error",
-        timer = {
-          interval = 1500,
+        timestamp = {
+          enabled = true,
           reset_on_idle = false,
           reset_on_change = false,
         },
         editor = {
-          image = nil,
+          icon = nil,
           client = "neovim",
           tooltip = "",
         },
@@ -23,7 +21,6 @@ return {
           show_cursor_position = false,
           swap_fields = false,
           swap_icons = true,
-          workspace_blacklist = {},
         },
         lsp = {
           show_problem_count = false,
@@ -31,34 +28,29 @@ return {
           scope = "workspace",
         },
         idle = {
-          enable = true,
+          enabled = true,
           show_status = true,
           timeout = 300000,
-          disable_on_focus = false,
-          text = "Idle",
+          ignore_focus = false,
+          details = "Idle",
           tooltip = "💤",
-        },
-        text = {
-          viewing = "Viewing {}",
-          editing = "Editing {}",
-          file_browser = "Browsing files in {}",
-          plugin_manager = "Managing plugins in {}",
-          lsp_manager = "Configuring LSP in {}",
-          vcs = "Committing changes in {}",
-          workspace = "",
         },
         buttons = {
           {
             label = "View Repository",
-            url = "git",
+            url = function(opts)
+              return opts.repo_url
+            end,
           },
+        },
+        text = {
+          workspace = "",
         },
         assets = {
           ranger = {
             name = "Ranger",
             tooltip = "ranger.nvim",
-            type = 1,
-            icon = "default",
+            type = "file_browser",
           },
         },
       }
